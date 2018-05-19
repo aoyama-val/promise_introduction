e377830dd46921466968b36c35d4e4b691345ed5
Aoyama, Shotaro <aoyama@val.co.jp>
2018-05-19 11:35:54 +0900

    reduceを使わない書き方を追加

--- 08.js
+++ 08.js
@@ -17,8 +17,23 @@ var f3 = function(result) {
     return result + 30;
 };
 
+// 1. reduceを使った書き方
+
 [f1, f2, f3].reduce(function(s, x) { return s.then(x); }, Promise.resolve(1))
 .then(function(result) {
     console.log("result", result);
     // => result 61
 });
+
+// 2. reduceを使わない書き方
+// （やっていることは1.とまったく同じです）
+
+var p = Promise.resolve(1);
+[f1, f2, f3].forEach(function(f) {
+    p = p.then(f);
+});
+p.then(function(result) {
+    console.log("result", result);
+    // => result 61
+});
+
